d1eb1ac477fad34113479b811e215234d8897008
notrodash <n@notr.co>
2014-02-16 13:06:15 +0000

    Added license to source files

--- pace4chrome/js/pace4chromebg.js
+++ pace4chrome/js/pace4chromebg.js
@@ -1,5 +1,28 @@
+/*
+ *	The MIT License (MIT)
+ *
+ *	Copyright (c) 2014 Niklas Vangerow
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
 var storage = chrome.storage.sync,
-tabs = chrome.tabs;
+	tabs = chrome.tabs;
 var hasSetting = false;
 var defaultCSSToInject = ".pace .pace-progress {\n    background: #29d;\n    position: fixed;\n    z-index: 2000;\n    top: 0;\n    left: 0;\n    height: 2px;\n    -webkit-transition: width 1s;\n    transition: width 1s;\n}\n\n.pace-inactive {\n    display: none;\n}";
 var cssToInject = defaultCSSToInject;
